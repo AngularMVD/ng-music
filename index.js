4df1004586501e3b9c6f43e26e332734e126c1b5
Santiago Ferreira <santiago.ferreira@wyeworks.com>
2016-04-14 20:27:58 -0300

    Redirect to root path on 404

--- index.js
+++ index.js
@@ -16,3 +16,7 @@ app.use('/api/*', function(req, res, next) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+app.use('/*', function(req, res) {
+  res.redirect('/')
+});
