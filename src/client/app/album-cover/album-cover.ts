9f6b257591bcd494a9fdb2198ecddd093c4db33d
Iran Reyes Fleitas <iranreyesfleitas@gmail.com>
2016-04-13 23:26:12 -0300

    Components and Bindings: Created the component <album-cover> and injected in <albums-page>

---
+++ src/client/app/album-cover/album-cover.ts
@@ -0,0 +1,15 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'album-cover',
+  templateUrl: 'app/album-cover/album-cover.html',
+  styleUrls: ['app/album-cover/album-cover.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class AlbumCover {
+
+  constructor() {}
+
+}
--- src/client/app/albums-page/albums-page.ts
+++ src/client/app/albums-page/albums-page.ts
@@ -1,12 +1,13 @@
 import {Component} from 'angular2/core';
 
+import {AlbumCover} from '../album-cover/album-cover';
 
 @Component({
   selector: 'albums-page',
   templateUrl: 'app/albums-page/albums-page.html',
   styleUrls: ['app/albums-page/albums-page.css'],
   providers: [],
-  directives: [],
+  directives: [AlbumCover],
   pipes: []
 })
 export class AlbumsPage {
