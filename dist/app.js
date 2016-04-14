168e658b00e08758feaffaf1f91558107668f682
Iran Reyes Fleitas <iranreyesfleitas@gmail.com>
2016-04-14 02:32:30 -0300

    Deploy: Added support for Heroku

---
+++ dist/app.js
@@ -0,0 +1,1 @@
+System.register(["angular2/platform/browser","./app/music"],function(exports_1,context_1){"use strict";var __moduleName=context_1&&context_1.id;var browser_1,music_1;return{setters:[function(browser_1_1){browser_1=browser_1_1},function(music_1_1){music_1=music_1_1}],execute:function(){browser_1.bootstrap(music_1.MusicApp,[])}}});
---
+++ index.js
@@ -0,0 +1,18 @@
+var request = require('request');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/dist'));
+app.use('/api/*', function(req, res, next) {
+	request('http://em-ng-workshop.herokuapp.com' + req.baseUrl, function(err, response, body) {
+		if(!err) {
+			res.send(body);
+		}
+	});
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
