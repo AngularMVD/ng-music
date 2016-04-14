a03c511f2b1984b2d101ceed10a97563b8222977
Iran Reyes Fleitas <iranreyesfleitas@gmail.com>
2016-04-14 01:13:24 -0300

    Routing: Linked to a /:id route from each album passing the id

--- src/client/app/album-cover/album-cover.ts
+++ src/client/app/album-cover/album-cover.ts
@@ -1,11 +1,12 @@
 import {Component, Input} from 'angular2/core';
+import {RouterLink} from 'angular2/router';
 
 @Component({
   selector: 'album-cover',
   templateUrl: 'app/album-cover/album-cover.html',
   styleUrls: ['app/album-cover/album-cover.css'],
   providers: [],
-  directives: [],
+  directives: [RouterLink],
   pipes: [],
 })
 export class AlbumCover {
