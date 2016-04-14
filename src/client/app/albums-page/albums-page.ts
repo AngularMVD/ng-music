92563bf6e34e6186b68d6312ee6657ca237d71fd
Iran Reyes Fleitas <iranreyesfleitas@gmail.com>
2016-04-14 02:04:44 -0300

    Services: Injecting the Albums service into Albums page and fetching all the albums

--- src/client/app/albums-page/albums-page.ts
+++ src/client/app/albums-page/albums-page.ts
@@ -1,20 +1,22 @@
 import {Component} from 'angular2/core';
 import {NgFor} from 'angular2/common';
-import {HTTP_PROVIDERS, Http} from 'angular2/http';
 
 import {AlbumCover} from '../album-cover/album-cover';
+import {AlbumsService} from '../albums-service/albums-service';
 
 @Component({
   selector: 'albums-page',
   templateUrl: 'app/albums-page/albums-page.html',
   styleUrls: ['app/albums-page/albums-page.css'],
-  providers: [HTTP_PROVIDERS],
+  providers: [AlbumsService],
   directives: [AlbumCover, NgFor],
   pipes: []
 })
 export class AlbumsPage {
 	albums: any = [];
 
-  constructor() {}
+  constructor(private _albumsService: AlbumsService) {
+		_albumsService.getAllAlbums().subscribe(albums => this.albums = albums);
+  }
 
 }
--- src/client/app/music.ts
+++ src/client/app/music.ts
@@ -1,5 +1,6 @@
 import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
+import {HTTP_PROVIDERS} from 'angular2/http';
 import {CliRouteConfig} from './route-config';
 
 import {AlbumsPage} from './albums-page/albums-page';
@@ -7,7 +8,7 @@ import {TracksPage} from './tracks-page/tracks-page';
 
 @Component({
   selector: 'music-app',
-  providers: [ROUTER_PROVIDERS],
+  providers: [ROUTER_PROVIDERS, HTTP_PROVIDERS],
   templateUrl: 'app/music.html',
   directives: [ROUTER_DIRECTIVES],
   pipes: []
