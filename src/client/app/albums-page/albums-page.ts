814313438cb25098a2094ff62439b49e0ae7ab81
Iran Reyes Fleitas <iranreyesfleitas@gmail.com>
2016-04-13 23:13:05 -0300

    Components and Bindings: Created the component <albums-page> and injected in <music-app>

---
+++ src/client/app/albums-page/albums-page.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+  selector: 'albums-page',
+  templateUrl: 'app/albums-page/albums-page.html',
+  styleUrls: ['app/albums-page/albums-page.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class AlbumsPage {
+
+  constructor() {}
+
+}
--- src/client/app/music.ts
+++ src/client/app/music.ts
@@ -2,11 +2,13 @@ import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 import {CliRouteConfig} from './route-config';
 
+import {AlbumsPage} from './albums-page/albums-page';
+
 @Component({
   selector: 'music-app',
   providers: [ROUTER_PROVIDERS],
   templateUrl: 'app/music.html',
-  directives: [ROUTER_DIRECTIVES],
+  directives: [ROUTER_DIRECTIVES, AlbumsPage],
   pipes: []
 })
 @RouteConfig([
